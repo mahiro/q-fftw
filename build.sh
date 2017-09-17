794900e86e44559f6b8678ba6544bbd79bdcf8f9
Mahiro Ando <ma514y@gmail.com>
2017-09-17 22:30:09 +0900

    add -O3

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ fi
 
 if [[ `uname` = 'Darwin' ]]; then
     set -x
-    gcc -bundle -undefined dynamic_lookup -DKXVER=3 $mflag \
+    gcc -bundle -undefined dynamic_lookup -DKXVER=3 $mflag -O3 \
         -I"$kdb_src/c/c" -I"$fftw_src/api" -L"$fftw_src/.libs" \
         qfftw.c -lfftw3 -lm -o qfftw.so "$@"
 elif [[ `uname` = MINGW* ]]; then
@@ -28,12 +28,12 @@ elif [[ `uname` = MINGW* ]]; then
 	    dlltool -v -l libq.a -d libq.def
     fi
     set -x
-    gcc -shared -static-libgcc -DKXVER=3 $mflag \
+    gcc -shared -static-libgcc -DKXVER=3 $mflag -O3 \
         -I"$kdb_src/c/c" -I"$fftw_src/api" -L"$fftw_src/.libs" -L"." \
         qfftw.c -lq -lfftw3 -lm -o qfftw.dll "$@"
 else
     set -x
-    gcc -shared -fPIC -DKXVER=3 -DKXVER=3 $mflag \
+    gcc -shared -fPIC -DKXVER=3 -DKXVER=3 $mflag -O3 \
         -I"$kdb_src/c/c" -I"$fftw_src/api" -L"$fftw_src/.libs" \
         qfftw.c -lfftw3 -lm -o qfftw.so "$@"
 fi
